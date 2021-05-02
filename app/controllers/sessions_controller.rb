ee156a64757b1e907ac98d6947c57ea5420551bf
Abner <abnercabrera28@gmail.com>
2021-05-02 17:21:45 -0400

    added the new and create actions as well as a logout method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,24 @@
 class SessionsController < ApplicationController
 
+    def new
+
+    end
+
+    def create
+        @user = User.find_by_username(params[:user][:username])
+
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to images_path
+        else
+            flash[:message] = "Incorrect Username or Password"
+            render :new
+        end
+    end
+
+    def logout
+        session.clear
+        redirect_to login_path
+    end
+
 end
