08159c58dde76bd9c27b05dbb42d158887af2475
Abner <abnercabrera28@gmail.com>
2021-05-02 16:53:58 -0400

    added static controller and home page, as well as stylesheets and a wrestler image

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+
+  root('static#home')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
