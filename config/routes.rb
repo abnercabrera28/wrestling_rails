4773887da110f8b20b4d40ec053ca059e08356c3
Abner <abnercabrera28@gmail.com>
2021-05-02 17:15:37 -0400

    added login routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'session#create'
+
   root('static#home')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
