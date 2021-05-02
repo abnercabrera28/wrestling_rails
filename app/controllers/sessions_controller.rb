e6a65e3482ab98172d8b191866e660c9bb8ab4ef
Abner <abnercabrera28@gmail.com>
2021-05-02 17:57:57 -0400

    added omniauth action as well as auth private method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,4 +21,29 @@ class SessionsController < ApplicationController
         redirect_to login_path
     end
 
+    def omniauth
+        
+        @user = User.find_or_create_by(username: auth[:info][:email]) do |u|
+            u.email = auth[:info][:email]
+            u.username = auth[:info][:email]
+            u.uid = auth[:uid]
+            u.provider = auth[:provider]
+            u.password = SecureRandom.hex(10)
+        end
+        
+        if @user.valid?
+            session[:user_id] = @user.id
+            redirect_to images_path
+        else
+            flash[:message] = "Incorrect credentials"
+            redirect_to login_path
+        end
+    end
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
+    end
+
 end
