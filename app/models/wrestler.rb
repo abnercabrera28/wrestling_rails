c78303baeb9363e4ef3ca0e51607094b8acc7c1d
Abner <abnercabrera28@gmail.com>
2021-05-02 14:11:13 -0400

    validated name, hails from, and birthdate

--- app/models/wrestler.rb
+++ app/models/wrestler.rb
@@ -1,4 +1,8 @@
 class Wrestler < ApplicationRecord
     belongs_to :user 
     belongs_to :team 
+
+    validates :name, presence: true
+    validates :hails_from, presence: true
+    validates :birthdate, presence: true
 end
