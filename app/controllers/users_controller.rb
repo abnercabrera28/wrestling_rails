ebabde85b392a8bdcbae3e9e11b7a541da2804b8
Abner <abnercabrera28@gmail.com>
2021-05-02 15:06:24 -0400

    added user_params private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,5 +14,9 @@ class UsersController < ApplicationController
         end
     end
 
-    
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :email)
+    end
 end
