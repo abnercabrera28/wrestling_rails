a3e8e99abfc09f6c83beea5d7a28570688d0e688
Abner <abnercabrera28@gmail.com>
2021-05-02 17:53:44 -0400

    added omniauth callback route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'session#create'
 
+  get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
+
   root('static#home')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
