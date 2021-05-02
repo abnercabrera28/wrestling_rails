05539f2865bb8f7a889beae53f67430b7f453860
Abner <abnercabrera28@gmail.com>
2021-05-02 14:05:20 -0400

    added username and password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
 
   has_many :wrestlers
   has_many :teams, through: :wrestlers
