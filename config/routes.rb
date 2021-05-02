f2741f93b4c346e28df9644f99173bc51cf3ae96
Abner <abnercabrera28@gmail.com>
2021-05-02 15:11:02 -0400

    added signup get and post routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :users
   resources :teams
   resources :wrestlers
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
