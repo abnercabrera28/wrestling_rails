b3a8a3bb90c9564fa49261006ed6fc8b680e7cfb
Abner <abnercabrera28@gmail.com>
2021-05-03 12:29:41 -0400

    commented out line 13

--- app/controllers/wrestlers_controller.rb
+++ app/controllers/wrestlers_controller.rb
@@ -10,7 +10,7 @@ class WrestlersController < ApplicationController
 
     def create
         @wrestler = current_user.wrestlers.build(wrestler_params)
-        @wrestler.team_id = Team.first.id
+        # @wrestler.team_id = Team.first.id
         if @wrestler.save
             redirect_to wrestler_path(@wrestler)
         else
