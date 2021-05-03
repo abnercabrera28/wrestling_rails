e97cd94c2fdba627cac4482929d9c114c3e15353
Abner <abnercabrera28@gmail.com>
2021-05-03 12:47:21 -0400

    gave wrestler and team models nested attributes

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,7 @@ class Team < ApplicationRecord
     has_many :wrestlers
     has_many :users, through: :wrestlers
 
+    accepts_nested_attributes_for :wrestlers
+
     validates :name, presence: true, uniqueness: true
 end
--- app/models/wrestler.rb
+++ app/models/wrestler.rb
@@ -1,6 +1,8 @@
 class Wrestler < ApplicationRecord
     belongs_to :user 
-    belongs_to :team 
+    belongs_to :team
+    
+    accepts_nested_attributes_for :team
 
     validates :name, presence: true
     validates :hails_from, presence: true
