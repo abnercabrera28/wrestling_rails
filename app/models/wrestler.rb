1f0dd26909704706400a5604d72a576736214969
Abner <abnercabrera28@gmail.com>
2021-05-03 19:19:14 -0400

    added most recent scope method

--- app/models/wrestler.rb
+++ app/models/wrestler.rb
@@ -2,12 +2,12 @@ class Wrestler < ApplicationRecord
     belongs_to :user 
     belongs_to :team
     
-    #accepts_nested_attributes_for :team
-
     validates :name, presence: true
     validates :hails_from, presence: true
     validates :birthdate, presence: true
-
+    
+    scope :most_recent, -> { order created_at: :desc }
+    
     def team_attributes=(attributes)
         if !attributes[:name].blank?
             self.team = Team.find_or_create_by(attributes)
