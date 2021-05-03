f01064d4a2d6836ff5715146a4be54231d6aef60
Abner <abnercabrera28@gmail.com>
2021-05-03 15:09:19 -0400

    added edit and update actions

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -21,6 +21,22 @@ class TeamsController < ApplicationController
         @team = Team.find_by_id(params[:id])
     end 
 
+    def edit
+        @team = Team.find_by_id(params[:id])
+    end
+
+    def update
+        @team = Team.find_by_id(params[:id])
+        @team.update(team_params)
+
+        if @team.valid?
+            redirect_to wrestler_path(@team)
+        else
+            render :edit
+        end
+    end
+
+
     private
 
     def team_params
