8ddb28ce38ba1c824816579a38bd1a2bb7246267
Abner <abnercabrera28@gmail.com>
2021-05-03 11:03:57 -0400

    added new and create actions

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,5 +4,12 @@ class TeamsController < ApplicationController
         @teams = Team.all
     end
 
-    
+    def new
+        @team = Team.new
+    end
+
+    def create
+        @team = Team.new
+        redirect_to team_path(@team)
+    end
 end
