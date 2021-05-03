2920b317bbc426164ada386bc3fecb6ba0ce0fd4
Abner <abnercabrera28@gmail.com>
2021-05-03 13:22:02 -0400

    added team_attributes method

--- app/models/wrestler.rb
+++ app/models/wrestler.rb
@@ -2,9 +2,15 @@ class Wrestler < ApplicationRecord
     belongs_to :user 
     belongs_to :team
     
-    accepts_nested_attributes_for :team
+    #accepts_nested_attributes_for :team
 
     validates :name, presence: true
     validates :hails_from, presence: true
     validates :birthdate, presence: true
+
+    def team_attributes=(attributes)
+        if !attributes[:name].blank?
+            self.team = Team.find_or_create_by(attributes)
+        end
+    end
 end
