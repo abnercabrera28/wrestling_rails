bdff7963c408a052c5465fc3a7642e0254f9fb34
Abner <abnercabrera28@gmail.com>
2021-05-03 21:42:23 -0400

    removed validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  
-  validates :username, presence: true, uniqueness: true
-  validates :password, presence: true
 
   has_many :wrestlers
   has_many :teams, through: :wrestlers
