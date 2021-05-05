963b64e24a07ca464f59721b4cea160ba95469a0
Abner <abnercabrera28@gmail.com>
2021-05-05 02:25:31 -0400

    added description attribute to teams and migrated it

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_174646) do
+ActiveRecord::Schema.define(version: 2021_05_05_062341) do
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
